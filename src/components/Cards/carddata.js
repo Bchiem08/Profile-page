2b89b5670d78aad663c4fbe9d1103f32b3b9d4be
Brian Chiem <brianfamily5@gmail.com>
2020-06-08 14:42:40 -0600

    Add Card page

---
+++ src/components/Cards/carddata.js
@@ -0,0 +1,9 @@
+const imagePath = process.env.PUBLIC_URL + "/photos";
+
+export const cards = [
+  {
+    image: imagePath + "/Me.png",
+    title: "",
+    skill: "",
+  },
+];
---
+++ src/components/Cards/infocard.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import "./carddata";
+import { Typography } from "@material-ui/core";
+
+export default function InfoCard(props) {
+  return (
+    <Card>
+      <CardMedia image={props.image} />
+      <CardContent>
+        <Typography>{props.title}</Typography>
+        <Typography>{props.skill}</Typography>
+      </CardContent>
+    </Card>
+  );
+}
